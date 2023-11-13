b6df1c418303ed951af6b891dc19c874974dc13f
EdgarSago <edgar13sago@gmail.com>
2023-11-13 14:06:40 -0600

    Footer design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 import './Footer.css';
 
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+import logo from '../../assets/cinema-icon.png';
+
 export default function Footer() {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className='footer'>
+            <ContentWrapper>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <h1>CinemaSagom</h1>
+                </div>
+                <ul className="menuItems">
+                    <li className="item">Terms Of Use</li>
+                    <li className="item">Privacy-Policy</li>
+                    <li className="item">About</li>
+                    <li className="item">Blog</li>
+                    <li className="item">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aspernatur optio. Laborum, dicta doloremque ipsum delectus quaerat vero cumque quo dolorum nihil commodi, dignissimos eveniet quasi quam error debitis officia.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaXTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaGithub />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
